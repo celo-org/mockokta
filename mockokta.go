8ffcfeb4fc9486661c5fffd73de33e702de1c65e
pputman-clabs <patrick.putman@clabs.co>
2023-01-07 00:09:44 -0600

    adding in user escalation functions and tests for them

---
+++ mockokta.go
@@ -0,0 +1,182 @@
+package mockokta
+
+import (
+	"context"
+	"fmt"
+	//"reflect"
+
+	"github.com/okta/okta-sdk-golang/v2/okta"
+	"github.com/okta/okta-sdk-golang/v2/okta/query"
+)
+
+var ADMIN_ROLES = []string{"SUPER_ADMIN", "ORG_ADMIN", "GROUP_ADMIN", "GROUP_MEMBERSHIP_ADMIN", "USER_ADMIN", "APP_ADMIN", "READ_ONLY_ADMIN", "MOBILE_ADMIN", "HELP_DESK_ADMIN", "REPORT_ADMIN", "API_ACCESS_MANAGEMENT_ADMIN", "CUSTOM"}
+
+// func NewClient(ctx context.Context, conf ...ConfigSetter) (context.Context, *Client, error) {
+type MockClient struct {
+	Group *GroupResource
+	User  *UserResource
+}
+
+func NewClient() *MockClient {
+	c := &MockClient{}
+	c.Group = &GroupResource{
+		Client: c,
+        GroupRoles: make(map[string][]*okta.Role),
+	}
+	c.User = &UserResource{
+		Client: c,
+	}
+	return c
+}
+
+type GroupResource struct {
+	Client     *MockClient
+	Groups     []*okta.Group
+	GroupRoles map[string][]*okta.Role
+	GroupUsers map[string][]string
+}
+
+func (g *GroupResource) CreateGroup(ctx context.Context, group okta.Group) (*okta.Group, *okta.Response, error) {
+
+	group.Id = fmt.Sprint(len(g.Groups)+1)
+	for _, x := range g.Groups {
+
+		if x.Profile.Name == group.Profile.Name {
+			return nil, nil, fmt.Errorf("unable to create group: group exists")
+		}
+	}
+
+	if len(group.Profile.Name) > 255 || len(group.Profile.Name) < 1 {
+		return nil, nil, fmt.Errorf("unable to create group: invalid name length")
+	}
+
+	g.Groups = append(g.Groups, &group)
+	return &group, nil, nil
+}
+
+func (g *GroupResource) ListGroups(context.Context, *query.Params) ([]*okta.Group, *okta.Response, error) {
+	return g.Groups, nil, nil
+}
+
+func NewGroup(groupName string) *okta.Group {
+	return &okta.Group{
+		Profile: &okta.GroupProfile{
+			Name: groupName,
+		},
+	}
+}
+
+/*
+	func (g *GroupResource) AddUserToGroup(ctx context.Context, groupId string, userId string) (*okta.Response, error) {
+		g.GroupUsers[groupId] = append(g.GroupUsers[groupId], userId)
+		return nil, nil
+	}
+*/
+func (g *GroupResource) AssignRoleToGroup(ctx context.Context, groupId string, assignRoleRequest okta.AssignRoleRequest, qp *query.Params) (*okta.Role, *okta.Response, error) {
+	if !SliceContainsString(ADMIN_ROLES, assignRoleRequest.Type) {
+		return nil, nil, fmt.Errorf("invalid role")
+	}
+    group, err := g.GetGroupById(groupId)
+    if err != nil {
+        return nil, nil, err
+    }
+
+    if g.GroupContainsRole(*group, assignRoleRequest.Type) {
+        return nil, nil, fmt.Errorf("group role exists")
+    }
+
+	role := NewRole(assignRoleRequest.Type)
+	role.Id = fmt.Sprintf("%v", len(g.GroupRoles)+1)
+	g.GroupRoles[group.Profile.Name] = append(g.GroupRoles[group.Profile.Name], &role)
+	return &role, nil, nil
+}
+
+func (g *GroupResource) ListGroupAssignedRoles(ctx context.Context, groupId string, qp *query.Params) ([]*okta.Role, *okta.Response, error) {
+        group, err := g.GetGroupById(groupId)
+        if err != nil {
+            return nil, nil, err
+        }
+
+		return g.GroupRoles[group.Profile.Name], nil, nil
+}
+
+func (g *GroupResource) GroupContainsRole(group okta.Group, roleType string) bool {
+	for _, groupRole := range g.GroupRoles[group.Profile.Name] {
+        
+		if groupRole.Type == roleType {
+			return true
+		}
+	}
+	return false
+}
+
+func (g *GroupResource) GetGroupById(groupId string) (*okta.Group, error) {
+    for _, group := range  g.Groups {
+        if group.Id == groupId {
+            return group, nil
+        }
+    }
+    return nil, fmt.Errorf("group not found")
+}
+
+type UserResource struct {
+	Client *MockClient
+	Users  []*okta.User
+}
+
+func (u *UserResource) CreateUser(userEmail string) (*okta.User, error) {
+    userId := fmt.Sprint(len(u.Users) + 1)
+
+    for _, u := range u.Users {
+        if (*u.Profile)["email"] == userEmail {
+            return nil, fmt.Errorf("user exists")
+        }
+    }
+
+    user := &okta.User{
+        Id: userId,
+        Profile: &okta.UserProfile{
+            "email": userEmail,
+        },
+    }
+
+    u.Users = append(u.Users, user)
+    return user, nil
+}  
+
+func (u *UserResource) ListUsers(context.Context, *query.Params) ([]*okta.User, *okta.Response, error) {
+	return u.Users, nil, nil
+}
+
+func (u *UserResource) GetUserByEmail(email string) (*okta.User, error) {
+    for _, user := range u.Users {
+        if (*user.Profile)["email"] == email {
+            return user, nil
+        }
+    }
+    return nil, fmt.Errorf("user not found")
+}
+
+func NewRole(roleType string) okta.Role {
+	return okta.Role{
+		Type: roleType,
+	}
+}
+
+func NewAssignRoleRequest(roleType string) okta.AssignRoleRequest {
+	return okta.AssignRoleRequest{
+		Type: roleType,
+	}
+}
+
+
+func SliceContainsString(slice []string, str string) bool {
+	for _, s := range slice {
+		if s == str {
+			return true
+		}
+	}
+	return false
+}
+
+
---
+++ mockokta_test.go
@@ -0,0 +1,241 @@
+package mockokta
+
+import (
+	"context"
+	"math/rand"
+	"reflect"
+	"testing"
+	"time"
+
+//	"github.com/okta/okta-sdk-golang/v2/okta/query"
+	"github.com/okta/okta-sdk-golang/v2/okta"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGroupResource_CreateGroup(t *testing.T) {
+	t.Run("should not create group with empty name", func(t *testing.T) {
+		groupNameArg := ""
+		client := NewClient()
+		group := NewGroup(groupNameArg)
+
+		_, _, err := client.Group.CreateGroup(context.TODO(), *group)
+
+		if err == nil {
+			t.Errorf("expected error but didn't get one")
+		}
+	})
+
+	t.Run("should not create group with invalid length", func(t *testing.T) {
+		groupNameArg := RandStringRunes(256)
+		client := NewClient()
+		group := NewGroup(groupNameArg)
+
+		_, _, err := client.Group.CreateGroup(context.TODO(), *group)
+
+		if err == nil {
+			t.Errorf("expected error but didn't get one")
+		}
+	})
+	t.Run("should not create group if it already exists", func(t *testing.T) {
+		groupNameArg := "TestGroup"
+		client := NewClient()
+		group := NewGroup(groupNameArg)
+
+		client.Group.CreateGroup(context.TODO(), *group)
+		_, _, err := client.Group.CreateGroup(context.TODO(), *group)
+
+		if err == nil {
+			t.Errorf("expected error but didn't get one")
+		}
+	})
+
+	t.Run("should create group", func(t *testing.T) {
+		groupNameArg := "TestGroup"
+		client := NewClient()
+		group := NewGroup(groupNameArg)
+
+		client.Group.CreateGroup(context.TODO(), *group)
+
+		want := group
+		got := client.Group.Groups[0]
+
+		if reflect.DeepEqual(got, want) {
+			t.Fatalf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestGroupResource_ListGroups(t *testing.T) {
+	group1NameArg := "TestGroup1"
+	group2NameArg := "TestGroup2"
+
+	client := NewClient()
+	group1 := NewGroup(group1NameArg)
+	group2 := NewGroup(group2NameArg)
+
+	client.Group.Groups = append(client.Group.Groups, group1, group2)
+
+	want := []*okta.Group{group1, group2}
+	got, _, _ := client.Group.ListGroups(context.TODO(), nil)
+
+	assert.ElementsMatch(t, got, want)
+}
+
+func TestGroupResource_AssignRoleToGroup(t *testing.T) {
+	t.Run("should not assign role with invalid name to group", func(t *testing.T) {
+		groupNameArg := "TestGroup"
+		roleArg := "Invalid_Role"
+        roleRequest := NewAssignRoleRequest(roleArg)
+
+		client := NewClient()
+		group, _, _ := client.Group.CreateGroup(context.TODO(), *NewGroup(groupNameArg))
+
+		_, _, err := client.Group.AssignRoleToGroup(context.TODO(), group.Id, roleRequest, nil)
+
+		if err == nil {
+			t.Errorf("Expected err, but didn't get one")
+		}
+	})
+
+	t.Run("should return error if group doesn't exist", func(t *testing.T) {
+        groupNameArg := "NonexistentGroup"
+       
+		client := NewClient()
+		roleRequest := RandAdminRoleRequest()
+
+		_, _, err := client.Group.AssignRoleToGroup(context.TODO(), groupNameArg, roleRequest, nil)
+
+		if err == nil {
+			t.Errorf("Expected err, but didn't get one")
+		}
+	})
+	t.Run("should return error if role exists for group", func(t *testing.T) {
+		groupNameArg := "TestGroup"
+		roleRequest := RandAdminRoleRequest()
+
+		client := NewClient()
+		group, _, _ := client.Group.CreateGroup(context.TODO(), *NewGroup(groupNameArg))
+
+		client.Group.AssignRoleToGroup(context.TODO(), group.Id, roleRequest, nil)
+		_, _, err := client.Group.AssignRoleToGroup(context.TODO(), group.Id, roleRequest, nil)
+
+		if err == nil {
+			t.Errorf("Expected err, but didn't get one")
+		}
+	})
+
+	t.Run("should assign role to group", func(t *testing.T) {
+		groupNameArg := "TestGroup"
+		roleRequest := RandAdminRoleRequest()
+		client := NewClient()
+		group, _, _ := client.Group.CreateGroup(context.TODO(), *NewGroup(groupNameArg))
+		client.Group.AssignRoleToGroup(context.TODO(), group.Id, roleRequest, nil)
+
+        got := client.Group.GroupContainsRole(*group, roleRequest.Type)
+
+        if got != true {
+            t.Errorf("expected group %+v to contain role %v but was not found", client.Group, roleRequest.Type)
+        }
+	})
+}
+
+func TestGroupResource_ListGroupAssignedRoles(t *testing.T) {
+	t.Run("should return error if the group doesn't exist", func(t *testing.T) {
+		groupIdArg := "NonexistentGroup"
+		roleRequest := RandAdminRoleRequest()
+
+		client := NewClient()
+	    _, _, err := client.Group.AssignRoleToGroup(context.TODO(), groupIdArg, roleRequest, nil)
+       
+		if err == nil {
+			t.Errorf("Expected err, but didn't get one")
+		}
+
+	})
+
+	t.Run("should list assigned roles", func(t *testing.T) {
+		groupNameArg := "TestGroup"
+
+		roleTypeArg1 := "SUPER_ADMIN"
+        roleTypeArg2 := "GROUP_ADMIN"
+
+        roleRequest1 := NewAssignRoleRequest(roleTypeArg1)
+        roleRequest2 := NewAssignRoleRequest(roleTypeArg2)
+       
+		client := NewClient()
+		group, _, _ := client.Group.CreateGroup(context.TODO(), *NewGroup(groupNameArg))
+
+		role1, _, _ :=  client.Group.AssignRoleToGroup(context.TODO(), group.Id, roleRequest1, nil)
+		role2, _, _ := client.Group.AssignRoleToGroup(context.TODO(), group.Id, roleRequest2, nil)
+       
+        want := []*okta.Role{role1, role2}
+        got, _, _ := client.Group.ListGroupAssignedRoles(context.TODO(), group.Id, nil)
+
+        if !reflect.DeepEqual(got, want) {
+            t.Errorf("expected roles %v to match %v", got, want)
+        }
+	})
+}
+
+func TestUserResource_CreateUser(t *testing.T) {
+    t.Run("should err if user exists", func(t *testing.T) {
+        userEmail := "TestUser@test.com"
+
+        client := NewClient()
+
+        client.User.CreateUser(userEmail)
+        _, err := client.User.CreateUser(userEmail)
+
+ 		if err == nil {
+			t.Errorf("expected error but didn't get one")
+		}
+    })
+
+    t.Run("should create user", func(t *testing.T) {
+        userEmail := "TestUser@test.com"
+
+        client := NewClient()
+
+        want, _ := client.User.CreateUser(userEmail)
+        got, _ := client.User.GetUserByEmail(userEmail)
+        
+        if !reflect.DeepEqual(got, want) {
+            t.Errorf("got %v want %v", got, want)
+        }
+    })
+}
+
+func TestUserResource_ListUsers(t *testing.T) {
+    client := NewClient()
+    userEmailArg1 := "TestUser1"
+    userEmailArg2 := "TestUser2"
+
+    user1, _ := client.User.CreateUser(userEmailArg1)
+    user2, _ := client.User.CreateUser(userEmailArg2)
+
+    want := []*okta.User{user1, user2}
+    got, _, _ := client.User.ListUsers(context.TODO(), nil)
+
+    
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got %v want %v", got, want)
+    }
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func RandAdminRoleRequest() okta.AssignRoleRequest {
+	rand.Seed(time.Now().UnixNano())
+    roleRequest := NewAssignRoleRequest(ADMIN_ROLES[rand.Intn(len(ADMIN_ROLES))])
+	return roleRequest
+}
+
